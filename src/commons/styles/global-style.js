4bd5bf147fe1f8013c76c8dfd227eecaaed7c431
Jorge Sidney <jorge.info88@gmail.com>
2022-05-22 02:26:38 -0300

    adicionando api

--- src/commons/styles/global-style.js
+++ src/commons/styles/global-style.js
@@ -2,23 +2,22 @@ import { createGlobalStyle } from  'styled-components'
 import CovidImg from '../../assets/img/covid.jpg'
 
 const globalStyle = createGlobalStyle`
-    
-* {
-    outline: none;
-    box-sizing: border-box;
-}
-
-    body {
-        line-heigth: normal
+    * {
+        outline: none;
+        box-sizing: border-box;
     }
 
-    html,  {
-    width:100%
-    min-height: 100%;
-    display: flex;
-    padding:0;
-    margin: 0;
-}
+    body {
+        line-height: normal
+    }
+
+    html, body {
+        width:100%
+        min-height: 100%;
+        display: flex;
+        padding:0;
+        margin: 0;
+    }
 
     #root {
         background: url(${CovidImg});
@@ -26,9 +25,19 @@ const globalStyle = createGlobalStyle`
         width: 100%;
         background-size: cover;
         background-position: center center;
-   }   
+    }   
 
+   .mb-2 {
+       margin-bottom: 16px;
+    }
    
+   .pt-2 {
+       padding-top: 16px;
+    }
+
+    .cursor{
+        cursor: pointer;
+    }
 `
 
 export default globalStyle
