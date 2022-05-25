4a8984d9b38e945f430dfe70a5f919e0945be68f
Jorge Sidney <jorge.info88@gmail.com>
2022-05-25 19:34:50 -0300

    criando os Cards, Boards e Panel.

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,10 +1,13 @@
 import React, { memo, useState, useCallback, useEffect } from 'react'
 import Api from '../../api'
+import Board from './components/Board'
+import Panel from './components/Panel'
 import { ContainerStyled } from './style'
 
 function Main () {
     const [data, setData] = useState({})
-    const [country, setCountry] = useState('portugal')
+    const [country, setCountry] = useState('germany')
+    const updateAt = new Date().toLocaleString()
 
     const getCovidData = useCallback((country) => {
         Api.getCountry(country)
@@ -13,14 +16,32 @@ function Main () {
 
     useEffect( () =>{
         getCovidData(country)
-    }, [getCovidData, country])       
+    }, [getCovidData, country])     
+    
+    const handleChange = ({ target }) => {
+        const country = target.value
+        setCountry(country)
+    }
+
+    
 
     return (
         <ContainerStyled>
             <div className='mb-2'>
+                <Panel 
+                    data={data}
+                    updateAt={updateAt}
+                    onChange={handleChange}
+                    country={country}
+                    getCovidData={getCovidData}               
+                />
+
 
             </div>
+            <Board data={data} />
+        
         </ContainerStyled>
+
     )
 }
 
